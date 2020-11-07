0e0c5183407bd3e52046b303e9978c5307b4d682
Ryan <ryancflam@hotmail.com>
2020-11-07 15:58:20 +0000

    haha update

--- Battleship.py
+++ Battleship.py
@@ -1,144 +1,165 @@
-import random
+from time import time
+from random import randint
+
+
 class Battleship:
     def __init__(self):
-        self.__shipsize=[4,3,3,2]
-        self.__shipcode=["B","C","S","D"]
-        self.__board=[[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "],[" "," "," "," "," "," "," "," "," "," "]]
-        self.__battleship=4
-        self.__cruiser=3
-        self.__submarine=3
-        self.__destroyer=2
-        self.__totalx=12
-        self.__turns=0
-    def displayboard(self,showships):
-        st=""
-        st+="\n  0123456789\n ┌----------┐\n"
-        for x in range(10):
-            st+=f"{x}|"
-            for y in range(10):
-                if showships==True:
-                    st+=f"{self.__board[x][y]}"
+        self.__boardSize = 10
+        self.__carrier = 5
+        self.__battleship = 4
+        self.__cruiser = 3
+        self.__submarine = 3
+        self.__destroyer = 2
+        self.__shipcode = ["S", "A", "B", "C", "D"]
+        self.__shipsize = [
+            self.__cruiser,
+            self.__carrier,
+            self.__battleship,
+            self.__cruiser,
+            self.__destroyer
+        ]
+        self.__ships = len(self.__shipcode)
+        self.__totalx = sum(self.__shipsize)
+        self.__attempts = 0
+        self.__start = time()
+        self.__gameWon = False
+        self.__board = self.__createBoard()
+        self.__placeShips()
+
+    def __createBoard(self):
+        board = []
+        for r in range(self.__boardSize):
+            board.append([])
+            for c in range(self.__boardSize):
+                board[r].append(".")
+        return board
+
+    def __placeShips(self):
+        for i in range(len(self.__shipcode)):
+            done = False
+            while not done:
+                rand1 = randint(0, self.__boardSize-1)
+                rand2 = randint(0, self.__boardSize-1)
+                direction = randint(0, 2)
+                piece = 0
+                problem = False
+                while piece < self.__shipsize[i] and not problem:
+                    if self.__board[rand1][rand2] == ".":
+                        self.__board[rand1][rand2] = self.__shipcode[i]
+                    else:
+                        problem = True
+                    if direction == 0:
+                        rand1 += 1
+                    else:
+                        rand2 += 1
+                    if rand1 > self.__boardSize-1 or rand2 > self.__boardSize-1:
+                        problem = True
+                    else:
+                        piece += 1
+                    if problem:
+                        for r in range(self.__boardSize):
+                            for c in range(self.__boardSize):
+                                if self.__board[r][c] == self.__shipcode[i]:
+                                    self.__board[r][c] = "."
+                if piece == self.__shipsize[i] and not problem:
+                    done = True
+
+    def displayBoard(self, showships):
+        output = "Current board:\n\n  "
+        for i in range(self.__boardSize):
+            output += " " + str(i)
+        output += "\n   -------------------\n"
+        for i in range(self.__boardSize):
+            output += f"{i}|"
+            for j in range(self.__boardSize):
+                if showships:
+                    output += f" {self.__board[i][j]}"
+                    try:
+                        output = output.replace("M", ".")
+                    except:
+                        pass
                 else:
-                    if self.__board[x][y]=="M":
-                        st+="M"
-                    elif self.__board[x][y]=="X":
-                        st+="X"
+                    if self.__board[i][j] == "M":
+                        output += " M"
+                    elif self.__board[i][j] == "X":
+                        output += " X"
                     else:
-                        st+=" "
-            st+="|\n"
-        st+=" └----------┘"
-        return st
-    def placeships(self):
-        for z in range(len(self.__shipcode)):
-            done=False
-            while done==False:
-                rand1=random.randint(0,9)
-                rand2=random.randint(0,9)
-                direction=random.randint(0,2)
-                piece=0
-                problem=False
-                while piece<self.__shipsize[z]and problem==False:
-                    if self.__board[rand1][rand2]==" ":
-                        self.__board[rand1][rand2]=self.__shipcode[z]
-                    else:
-                        problem=True
-                    if direction==0:
-                        rand1+=1
-                    else:
-                        rand2+=1
-                    if rand1>9or rand2>9:
-                        problem=True
-                    else:
-                        piece+=1
-                    if problem==True:
-                        for row in range(10):
-                            for column in range(10):
-                                if self.__board[row][column]==self.__shipcode[z]:
-                                    self.__board[row][column]=" "
-                if piece==self.__shipsize[z]and problem==False:
-                    done=True
-    def taketurn(self,ships):
+                        output += " ."
+            output += "\n"
+        return output
+
+    def getShipcount(self):
+        return self.__ships
+
+    def getAttempts(self):
+        return self.__attempts
+
+    def getWonBool(self):
+        return self.__gameWon
+
+    def getTime(self):
+        return int(time() - self.__start)
+
+    def takeTurn(self, row, col):
         try:
-            row=int(input("\nEnter row number (0-9): "))
-            if row is None:
-                return None
-            while row<0 or row>9:
-                row=int(input("\n[ERROR] Row number must be 0-9 inclusive.\n\nEnter row number (0-9): "))
-                if row is None:
-                    return None
-            col=int(input("\nEnter column number (0-9): "))
-            if col is None:
-                return None
-            while col<0 or col>9:
-                col=int(input("\n[ERROR] Column number must be 0-9 inclusive.\n\nEnter column number (0-9): "))
-                if col is None:
-                    return None
-            if self.__board[row][col]=="M"or self.__board[row][col]=="X":
-                print("\nYou have wasted a turn.")
-                self.__turns+=1
-            if self.__board[row][col]==" ":
-                self.__turns+=1
-                print("\nMiss.")
-                self.__board[row][col]="M"
-            if self.__board[row][col]=="B":
-                print("\nHit.")
-                self.__board[row][col]="X"
-                self.__battleship-=1
-                self.__totalx-=1
-                self.__turns+=1
-                if self.__battleship==0:
-                    print("\nYou have sunk the battleship.")
-                    ships-=1
-                if self.__totalx==0:
-                    print(f"\nGame required {self.__turns} turns.")
-            if self.__board[row][col]=="S":
-                print("\nHit.")
-                self.__board[row][col]="X"
-                self.__submarine-=1
-                self.__totalx-=1
-                self.__turns+=1
-                if self.__submarine==0:
-                    print("\nYou have sunk the submarine.")
-                    ships-=1
-                if self.__totalx==0:
-                    print(f"\nGame required {self.__turns} turns.")
-            if self.__board[row][col]=="C":
-                print("\nHit.")
-                self.__board[row][col]="X"
-                self.__cruiser-=1
-                self.__totalx-=1
-                self.__turns+=1
-                if self.__cruiser==0:
-                    print("\nYou have sunk the cruiser.")
-                    ships-=1
-                if self.__totalx==0:
-                    print(f"\nGame required {self.__turns} turns.")
-            if self.__board[row][col]=="D":
-                print("\nHit.")
-                self.__board[row][col]="X"
-                self.__destroyer-=1
-                self.__totalx-=1
-                self.__turns+=1
-                if self.__destroyer==0:
-                    print("\nYou have sunk the destroyer.")
-                    ships-=1
-                if self.__totalx==0:
-                    print(f"\nGame required {self.__turns} turns.")
-            return ships
+            row, col = int(row), int(col)
+            if self.__board[row][col] == "M" or self.__board[row][col] == "X":
+                msg = "wasted a turn"
+            elif self.__board[row][col] == ".":
+                self.__board[row][col] = "M"
+                msg = "missed"
+            else:
+                msg = "hit"
+                self.__totalx -= 1
+                if self.__board[row][col] == "B":
+                    self.__battleship -= 1
+                    if self.__battleship == 0:
+                        self.__ships -= 1
+                        msg = "sunk the Battleship"
+                if self.__board[row][col] == "S":
+                    self.__submarine -= 1
+                    if self.__submarine == 0:
+                        self.__ships -= 1
+                        msg = "sunk the Submarine"
+                if self.__board[row][col] == "C":
+                    self.__cruiser -= 1
+                    if self.__cruiser == 0:
+                        self.__ships -= 1
+                        msg = "sunk the Cruiser"
+                if self.__board[row][col] == "D":
+                    self.__destroyer -= 1
+                    if self.__destroyer == 0:
+                        self.__ships -= 1
+                        msg = "sunk the Destroyer"
+                if self.__board[row][col] == "A":
+                    self.__carrier -= 1
+                    if self.__carrier == 0:
+                        self.__ships -= 1
+                        msg = "sunk the Aircraft Carrier"
+                self.__board[row][col] = "X"
+                if self.__totalx == 0:
+                    self.__gameWon = True
+            self.__attempts += 1
+            return msg
         except ValueError:
-            print("\nYou have left Battleship.")
-            return None
-        except KeyboardInterrupt:
-            return None
-    def turn(self):
-        ships=4
-        while ships>0:
-            print(self.displayboard(False))
-            ships=self.taketurn(ships)
-            if ships is None:
-                return
-    def game(self):
-        print("\nWelcome to Battleship. Enter any value other than the digits 0-9 to quit.")
-        self.placeships()
-        self.turn()
-Battleship().game()
+            raise Exception("Must be string")
+        except IndexError:
+            raise Exception("Out of range")
+
+
+game = Battleship()
+
+
+while not game.getWonBool():
+    print(game.displayBoard(False))
+    shipCount = game.getShipcount()
+    print(str(shipCount) + " ship(s) left; attempt " + str(game.getAttempts() + 1))
+    row = input("\nEnter row: ")
+    col = input("Enter column: ")
+    try:
+        print("\nYou have " + game.takeTurn(row, col) + "\n")
+    except Exception as ex:
+        print(ex)
+
+
+print(str(game.getTime()) + " seconds lasted")
